25587360a691d29cb60f186d77f649170d35e832
kleidimilson <kleidimilson@gmail.com>
2019-12-07 11:27:57 -0300

    php

--- quem-somos.php
+++ quem-somos.php
@@ -59,7 +59,7 @@
 			<div class="row">
 					<div class="col-lg-6">
 							<h1><span id="span-quemSomos">Quem</span> somos</h1>
-							<p>Somos um site de divulgação de cursos online, que tem como objetivo facilitar o consumo de cursos no Piauí. Nosso cursos são escolhidos a dedo para que você tenha uma excelente experiência</p>
+							<p>Somos um site de divulgação de cursos online, que tem como objetivo facilitar o consumo de cursos no Piauí. Nosso cursos são escolhidos a dedo para que você tenha uma excelente experiência.</p>
 							<a href="contato.html" class="site-btn">Contato</a>
 					</div>
 				<div class="col-lg-6">
