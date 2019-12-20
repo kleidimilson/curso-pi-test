7fbbc940e059ddf32d5b84a89537a0dfe4d293b2
kleidimilson <kleidimilson@gmail.com>
2019-12-20 17:00:03 -0300

    novo produto

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<meta charset="UTF-8">
 	<meta name="google-site-verification" content="_wQri2oXcORWiwM621v0yxSrTU8u10NtlxFWOHQX7lk" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="Plataforma de cursos online do Piauí, aqui tem cursos de qualidade">
+	<meta name="description" content="Plataforma de cursos online do Piauí, aqui tem cursos de qualidade. Fazemos uma seleção de cursos para você, todos os cursos são de extrema qualidade com excelentes professores preocupados em te passar os melhores conteúdos.">
 	<meta name="keywords" content="cursos online, cursos pi, cursos no piaui, piauí cursos, plataforma de cursos no piaui, CURSOSPI, cursos piaui, cursos PI, cursos online, cursos com certificado, Pi, PI">
 	<meta name="robots" content="">
 	<meta name="revisit-after" content="1 day">
